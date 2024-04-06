6b3037cd5dc341fe288de97e5087d799fc3afb4a
Martin Roberto Gaido <119458663+martingaidoo@users.noreply.github.com>
2024-04-06 01:10:15 +0000

    se agrega algo

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Martin Gaido",
+  title: "Estudiante de Ingeniería en Sistemas",
+  email: "martingaido00@gmail.com",
+  gitHub: "martingaidoo",
+  instagram: "martingaidoo",
+  linkedIn: "martin-gaido",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
