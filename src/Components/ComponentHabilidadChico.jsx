704fa2b1d58f91ff93e1c0c74cbee3aeb7055c1c
martingaidoo <martingaido00@gmail.com>
2024-04-18 10:46:13 -0300

    nada3

--- src/Components/ComponentHabilidadChico.jsx
+++ src/Components/ComponentHabilidadChico.jsx
@@ -4,6 +4,7 @@ import "../styles/styleProyect.css"
 export const ComponentHabilidadChico = (props) => {
   const {title, imagen} = props
   return (
+    
     <div className='BoxChico'>
       <img src={imagen} alt='lenguaje' />
       <p>{title}</p>
